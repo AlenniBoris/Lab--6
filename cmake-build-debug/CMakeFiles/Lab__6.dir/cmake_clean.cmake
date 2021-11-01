713aa346db2140eddf9e85bb7722d3537865d11d
Boris Alennikov <AlennikovBoris@gmail.com>
2021-11-01 16:46:46 +0300

    test1

---
+++ cmake-build-debug/CMakeFiles/Lab__6.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab__6.dir/functions__solution.cpp.obj"
+  "CMakeFiles/Lab__6.dir/main.cpp.obj"
+  "Lab__6.exe"
+  "Lab__6.exe.manifest"
+  "Lab__6.lib"
+  "Lab__6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab__6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
